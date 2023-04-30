518f5e6a27acc0c7429563917c5819dae285e3d4
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-04-30 03:30:30 +0300

    ya rab ⚒️🙏🙏🙏🙏

--- Assigment 2/code.m
+++ Assigment 2/code.m
@@ -1,12 +1,13 @@
 %% initial
 % array filled with zeros size of 10000
-array = zeros(1, 10000);
+size_array=10000;
+array = zeros(1, size_array);
 % create 5000 random index
-random_Indices = randperm(10000, 5000);
+random_Indices = randperm(size_array, 5000);
 array(random_Indices)=1;
 
 %% g array of size 10 times the size of array
-g = 10 * zeros(1, 10000);
+g = 10 * zeros(1, size_array);
 
 for i = 1:length(array)
     if(array(i)==1)
@@ -15,6 +16,7 @@ for i = 1:length(array)
        g(10*(i-1)+1:10*i)=-1;
     end
 end
+
 %% SNR
 % range of SNR
 snr_range = -10:1:20;
@@ -33,15 +35,15 @@ filter_1 = ones(10, 1);
 
 % The receive filter ?(?) is not existent (i.e. ?(?) = ?(?))
 filter_2 = zeros(10, 1); 
-filter_2(5) = 1; 
+filter_2(1) = 10; 
 
 
 % The receive filter ?(?) has the following impulse response
-filter_3 = zeros(1, 10);
+filter_3 = zeros(10, 1);
 % slope of the line
 slope = (sqrt(3) - 0) / (10 - 0);
-for x = 0:9
-    filter_3(x + 1) = slope * x; % store value in specific index in filter 3
+for x = 1:10
+    filter_3(x ) = slope * x; % store value in specific index in filter 3
 end
 
 % applay each filter to noice signal
@@ -59,24 +61,24 @@ for i = 1:length(snr_range)
 end
 
 figure(1)
-t1 = 1:100000;
+t1 = 1:(size_array*10);
 plot(t1,  signals_filters(:,i,1))
 title('h(t)= unit energy')
 xlabel('time (ms)')
 ylabel('recieve filter output')
 figure(2)
 plot(t1,  signals_filters(:,i,2))
-title('h(t)= unit energy')
+title('h(t)= not existent')
 xlabel('time (ms)')
 ylabel('recieve filter output')
 figure(3)
 plot(t1,  signals_filters(:,i,3))
-title('h(t)= unit energy')
+title('h(t)= triangle shape')
 xlabel('time (ms)')
 ylabel('recieve filter output')
 %% sampling
 % Sample the signals after applaying 3 filters
-samples= zeros(length(g)/10, length(snr_range), 3);
+samples= zeros(length(array), length(snr_range), 3);
 for i = 1:length(snr_range)
     for z = 1:3
         signal_filters = signals_filters(:,i,z);
@@ -84,16 +86,20 @@ for i = 1:length(snr_range)
     end
 end
 % Decode 
-Signals_output = zeros(size(samples));
+Signals_output = zeros(length(g)/10, length(snr_range), 3);
 Signals_output(samples >= 0) = 1;
 Signals_output(samples < 0) = 0;
 
 %% Calculate the theoretical and simulated BERs
 bits_num = length(g)/10;
 % Calculate theoretical BER
-BER_theoretical = zeros(size(snr_range));
+BER_theoretical_1 = zeros(1,length(snr_range));
+BER_theoretical_2 = zeros(1,length(snr_range));
+BER_theoretical_3 = zeros(1,length(snr_range));
 for i = 1:length(snr_range)
-    BER_theoretical(i) = 0.5*erfc(sqrt(10^(snr_range(i)/10)));
+     BER_theoretical_1(i)= erfc(sqrt(10^(snr_range(i)/10))^0.5);
+     BER_theoretical_2(i)=BER_theoretical_1(i);
+    BER_theoretical_3(i)= erfc((3^0.5/2) *sqrt(10^(snr_range(i)/10))^0.5);
 end
 % Calculate simulated BER
 BER_simulated = zeros(length(snr_range), 3);
@@ -106,63 +112,21 @@ end
 
 % % Plot the theoretical and simulated BERs vs SNR
 figure
-semilogy(snr_range, BER_theoretical, 'b-o')
+
+semilogy(snr_range, BER_theoretical_1, 'y-o')
 hold on
-semilogy(snr_range, BER_simulated(:,1), 'r-x')
-semilogy(snr_range, BER_simulated(:,2), 'g-d')
-semilogy(snr_range, BER_simulated(:,3), 'm-s')
+semilogy(snr_range, BER_theoretical_2, 'b-o')
+hold on
+semilogy(snr_range, BER_theoretical_3, 'c-o')
+hold on
+semilogy(snr_range, BER_simulated(:,1), 'r-o')
+hold on
+semilogy(snr_range, BER_simulated(:,2), 'g-o')
+hold on
+semilogy(snr_range, BER_simulated(:,3), 'm-o')
 hold off
 xlabel('SNR (dB)')
 ylabel('Bit Error Rate (BER)')
-legend('theo', 'Simulated (filter 1)', 'Simulated (filter 2)', 'Simulated (filter 3)')
-% Plot the theoretical BER vs SNR
+legend('theoretical 1','theoretical 2','theoretical 3', 'Simulated (filter 1)', 'Simulated (filter 2)', 'Simulated (filter 3)')
 
-
-figure
-subplot(2,1,1)
-semilogy(snr_range, BER_theoretical, 'b-o')
-xlabel('SNR (dB)')
-ylabel('Bit Error Rate (BER)')
-title('Theoretical BER')
-
-% % Plot the simulated BERs vs SNR
-subplot(2,1,2)
-semilogy(snr_range, BER_simulated(:,1), 'r-x')
-hold on
-semilogy(snr_range, BER_simulated(:,2), 'g-d')
-semilogy(snr_range, BER_simulated(:,3), 'm-s')
-hold off
-xlabel('SNR (dB)')
-ylabel('Bit Error Rate')
-legend('Simulated (filter 1)', 'Simulated (filter 2)', 'Simulated (filter 3)')
-title('Simulated BER')
-
-figure
-subplot(2,2,1)
-semilogy(snr_range, BER_theoretical, 'b-o')
-xlabel('SNR (dB)')
-ylabel('Bit Error Rate')
-title('Theoretical BER')
-grid on
-
-subplot(2,2,2)
-semilogy(snr_range, BER_simulated(:,1), 'r-x')
-xlabel('SNR (dB)')
-ylabel('Bit Error Rate')
-title('Simulated BER (filter 1)')
-grid on
-
-subplot(2,2,3)
-semilogy(snr_range, BER_simulated(:,2), 'g-d')
-xlabel('SNR (dB)')
-ylabel('Bit Error Rate')
-title('Simulated BER (filter 2)')
-grid on
-
-subplot(2,2,4)
-semilogy(snr_range, BER_simulated(:,3), 'm-s')
-xlabel('SNR (dB)')
-ylabel('Bit Error Rate')
-title('Simulated BER (filter 3)')
-grid on
 
