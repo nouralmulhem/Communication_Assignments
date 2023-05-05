45f7be664da25f1fde8b58f5566b0f3a2b4639dc
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-05-05 15:35:57 +0300

    add assignment 2

--- Assigment 2/code.m
+++ Assigment 2/code.m
@@ -1,132 +1,174 @@
-%% initial
-% array filled with zeros size of 10000
-size_array=10000;
-array = zeros(1, size_array);
-% create 5000 random index
-random_Indices = randperm(size_array, 5000);
-array(random_Indices)=1;
+%% constants
+num_bits = 10;
+num_of_samples = 10;
 
-%% g array of size 10 times the size of array
-g = 10 * zeros(1, size_array);
 
-for i = 1:length(array)
-    if(array(i)==1)
-       g(10*(i-1)+1:10*i)=1;
-    else
-       g(10*(i-1)+1:10*i)=-1;
-    end
-end
+%% generate random bits
+random_Indices = randperm(num_bits, num_bits/2);
+array= ones(num_bits,1);
+array(random_Indices)=-1;
 
-%% SNR
+array(1)=-1;
+array(2)=-1;
+array(3)=1;
+array(4)=1;
+array(5)=-1;
+array(6)=-1;
+array(7)=-1;
+array(8)=-1;
+array(9)=1;
+array(10)=1;
 % range of SNR
 snr_range = -10:1:20;
-% create signals_noise array
-signals_noise = zeros(length(g), length(snr_range));
-% add noise
+%% receive with matched filter
+filter_matched = ones(1,num_of_samples);
+energy=1;
+
 for i = 1:length(snr_range)
-    signals_noise(:,i) = awgn(g, snr_range(i), 'measured');
+   snr = 10 ^(snr_range(i)/10);
+   signal_with_noise_1=noise(num_bits,num_of_samples,array,energy/(2.0*snr));
+   [filtered_samples_1,filtered_bits_1]=applay_filter(num_bits,num_of_samples,signal_with_noise_1,filter_matched);
 end
-
-
-%% The receive filters
-% The receive filter ?(?) is a matched filter with unit energy 
-filter_1 = ones(10, 1);  
-
-
-% The receive filter ?(?) is not existent (i.e. ?(?) = ?(?))
-filter_2 = zeros(10, 1); 
-filter_2(1) = 10; 
-
-
-% The receive filter ?(?) has the following impulse response
-filter_3 = zeros(10, 1);
-% slope of the line
-slope = (sqrt(3) - 0) / (10 - 0);
-for x = 1:10
-    filter_3(x ) = slope * x; % store value in specific index in filter 3
-end
-
-% applay each filter to noice signal
-signals_filters = zeros(length(g), length(snr_range), 3);
-for i = 1:length(snr_range)
-    
-    % Convolve with filter 1
-    signals_filters(:,i,1) = conv(signals_noise(:,i), filter_1, 'same');
-
-    % Convolve with filter 2
-    signals_filters(:,i,2) = conv(signals_noise(:,i), filter_2, 'same');
-
-    % Convolve with filter 3 
-    signals_filters(:,i,3) = conv(signals_noise(:,i), filter_3, 'same');
-end
-
+% ploting
+out_1 = reshape(filtered_bits_1, [], 1);
 figure(1)
-t1 = 1:(size_array*10);
-plot(t1,  signals_filters(:,i,1))
-title('h(t)= unit energy')
+t1 = 1:(length(out_1));
+plot(t1,  out_1)
+title('output from matched filter')
 xlabel('time (ms)')
-ylabel('recieve filter output')
+ylabel('recieve filter output (bit value)')
+
+%% receive with no filter
+filter_no_filter = zeros(1,num_of_samples);
+filter_no_filter(num_of_samples/2)=10;
+for i = 1:length(snr_range)
+   snr = 10 ^(snr_range(i)/10);
+   signal_with_noise_2=noise(num_bits,num_of_samples,array,energy/(2.0*snr));
+   [filtered_samples_2,filtered_bits_2]=applay_filter(num_bits,num_of_samples,signal_with_noise_2,filter_no_filter);
+end
+% ploting
+out_2 = reshape(filtered_bits_2, [], 1);
 figure(2)
-plot(t1,  signals_filters(:,i,2))
-title('h(t)= not existent')
+t2 = 1:(length(out_2));
+plot(t2,  out_2)
+title('output from no filter')
 xlabel('time (ms)')
-ylabel('recieve filter output')
+ylabel('recieve filter output (bit value)')
+
+% receive with tringle
+t3 = 0:1: 10 - 1;
+filter_tringle =(1.732 / 10) * t3;
+for i = 1:length(snr_range)
+   snr = 10 ^(snr_range(i)/10);
+   signal_with_noise_3=noise(num_bits,num_of_samples,array,energy/(2.0*snr));
+   [filtered_samples_3,filtered_bits_3]=applay_filter(num_bits,num_of_samples,signal_with_noise_3,filter_tringle);
+end
+% ploting
+out_3 = reshape(filtered_bits_3, [], 1);
 figure(3)
-plot(t1,  signals_filters(:,i,3))
-title('h(t)= triangle shape')
+t3 = 1:(length(out_3));
+plot(t3,  out_3)
+title('output from tringle filter')
 xlabel('time (ms)')
-ylabel('recieve filter output')
-%% sampling
-% Sample the signals after applaying 3 filters
-samples= zeros(length(array), length(snr_range), 3);
+ylabel('recieve filter output (bit value)')
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+num_bits_2 = 100000;
+random_Indices_2 = randperm(num_bits_2, 1);
+array_2= ones(num_bits_2,1);
+array_2(random_Indices_2)=-1;
+BER_experimental_1=zeros(length(snr_range),1);
+BER_theortical_1 =zeros(length(snr_range),1);
+BER_experimental_2=zeros(length(snr_range),1);
+BER_theortical_2 =zeros(length(snr_range),1);
+BER_experimental_3=zeros(length(snr_range),1);
+BER_theortical_3 =zeros(length(snr_range),1);
 for i = 1:length(snr_range)
-    for z = 1:3
-        signal_filters = signals_filters(:,i,z);
-        samples(:,i,z) = signal_filters(5:10:end);
-    end
-end
-% Decode 
-Signals_output = zeros(length(g)/10, length(snr_range), 3);
-Signals_output(samples >= 0) = 1;
-Signals_output(samples < 0) = 0;
-
-%% Calculate the theoretical and simulated BERs
-bits_num = length(g)/10;
-% Calculate theoretical BER
-BER_theoretical_1 = zeros(1,length(snr_range));
-BER_theoretical_2 = zeros(1,length(snr_range));
-BER_theoretical_3 = zeros(1,length(snr_range));
-for i = 1:length(snr_range)
-     BER_theoretical_1(i)= erfc(sqrt(10^(snr_range(i)/10))^0.5);
-     BER_theoretical_2(i)=BER_theoretical_1(i);
-    BER_theoretical_3(i)= erfc((3^0.5/2) *sqrt(10^(snr_range(i)/10))^0.5);
-end
-% Calculate simulated BER
-BER_simulated = zeros(length(snr_range), 3);
-for i = 1:length(snr_range)
-    for j = 1:3
-        error = sum(xor(Signals_output(:,i,j)', array));
-        BER_simulated(i,j) = error/bits_num;
-    end
+    snr = 10 ^(snr_range(i)/10);
+    signal_with_noise=noise(num_bits_2,num_of_samples,array_2,energy/(2.0*snr));
+    [filtered_samples_1,filtered_bits_1]=applay_filter(num_bits_2,num_of_samples,signal_with_noise,filter_matched);
+    [filtered_samples_2,filtered_bits_2]=applay_filter(num_bits_2,num_of_samples,signal_with_noise,filter_no_filter);
+    [signal_with_noise_3,filtered_bits_3]=applay_filter(num_bits_2,num_of_samples,signal_with_noise,filter_tringle);
+    BER_experimental_1(i)=simulated_BER(array_2,filtered_samples_1);
+    BER_experimental_2(i)=simulated_BER(array_2,filtered_samples_2);
+    BER_experimental_3(i)=simulated_BER(array_2,signal_with_noise_3);
+    BER_theortical_1(i)=0.5*erfc(sqrt(snr));
+    BER_theortical_2(i)=0.5*erfc(sqrt(snr));
+    BER_theortical_3(i)=0.5*erfc((sqrt(3)/(2)*sqrt(snr)));
 end
 
-% % Plot the theoretical and simulated BERs vs SNR
-figure
+figure(4)
 
-semilogy(snr_range, BER_theoretical_1, 'y-o')
+semilogy(snr_range, BER_theortical_1, 'b-')
 hold on
-semilogy(snr_range, BER_theoretical_2, 'b-o')
+semilogy(snr_range, BER_experimental_1, 'm--')
+% xlabel('SNR (dB)')
+% ylabel('Bit Error Rate (BER)')
+% legend('theoretical 1', 'Simulated (filter 1)')
+% figure(5)
+
 hold on
-semilogy(snr_range, BER_theoretical_3, 'c-o')
+semilogy(snr_range, BER_theortical_2, 'r:')
 hold on
-semilogy(snr_range, BER_simulated(:,1), 'r-o')
+semilogy(snr_range, BER_experimental_2,'g--')
+% xlabel('SNR (dB)')
+% ylabel('Bit Error Rate (BER)')
+% legend('theoretical 2', 'Simulated (filter 2)')
+
+% figure(6)
 hold on
-semilogy(snr_range, BER_simulated(:,2), 'g-o')
+semilogy(snr_range, BER_theortical_3, 'c--')
 hold on
-semilogy(snr_range, BER_simulated(:,3), 'm-o')
+semilogy(snr_range, BER_experimental_3,'y--')
 hold off
+ylim([10^-4 0.5])
 xlabel('SNR (dB)')
 ylabel('Bit Error Rate (BER)')
-legend('theoretical 1','theoretical 2','theoretical 3', 'Simulated (filter 1)', 'Simulated (filter 2)', 'Simulated (filter 3)')
+legend('theoretical 1','Simulated (filter 1)','theoretical 2',  'Simulated (filter 2)','theoretical 3','Simulated (filter 3)')
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% utilts 
+%% add noise to signal
+%it is expanding generated_bits so each sample change to train of pulses to discretization the pulse
+%then adding gussian noise for expanded array.
+function [signal_with_noise] = noise(num_of_bits,num_of_samples,indx,sigma)
+   X = normrnd(0, sqrt(sigma), [1,num_of_bits*num_of_samples]);
+   signal_with_noise = ones(length(indx), num_of_samples);
+   
+   for i= 1:(length(indx))
+        signal_with_noise(i, :) =signal_with_noise(i, :)* indx(i); 
+        signal_with_noise(i, :) = signal_with_noise(i, :)+X((num_of_samples)*(i-1)+1:(num_of_samples)*(i));
+   end
+end
+%% applay Filter
+% it calculates convolution for received_filter and each sample
+function [result_sampled,result] = applay_filter(num_bits,num_of_samples,signal_with_noise,filter)
+    result_sampled = zeros(num_bits,1);
+    signal_with_noise_reshape=reshape(signal_with_noise.',1,[]);
+    result = conv(signal_with_noise_reshape, filter);
+    for i=0:(num_bits-1)
+        result_sampled(i+1) = result((num_of_samples - 1) + num_of_samples * i+1);
+    end
+end
+%% calcute simulated BER
+% it calculates convolution for received_filter and each sample
+function [BER_value] = simulated_BER(true_values,result)
+
+  %  result_reshape=reshape(result.',1,[]);
+    received_samples = ones(size(true_values));
+    received_samples = received_samples + (-2 * (result < 0));
+    error_probability = sum(received_samples ~= true_values);
+    BER_value = error_probability / size(true_values, 1);
+end
+
+% ?Q5:
+% BER is decreasing function of ?/??
+% because by increasing ?/?? we increase the transmitted energy related to noise energy. The transmitted signal was not 
+% significantly impacted by noise. but if noise energy close to transmitted energy then noise easily destroy the signal
+
+% Q6:
+% matched filter has the lowest BER 
+% because of multipling high values in signal by high values in filter than in thrsholding step then you can easly detect it is high value,
+% the same thing to low values
+% but other filter doesn't multply signal values with suitable values 
